ea72e066ea625a6fec682bf83f072d78fb818e6e
Cynthia Blaise <cynthiablaisein@gmail.com>
2026-01-22 16:31:09 -0500

    Updated R code files

--- scripts/r/ANOVAcode.R
+++ scripts/r/ANOVAcode.R
@@ -1,9 +1,9 @@
 ## Code for running the mixed effects AVNOA and one-way ANOVA 
 
 # Packages
+library(arrow)
 
-# Convert to long format
-
+Dataset_clean <- read_parquet("~/Desktop/Projects/Programming/PSYC499/data_clean/ANOVA_dataset.parquet")
 # Load models
 
 # Output
--- scripts/r/LGMCode.R
+++ scripts/r/LGMCode.R
@@ -1,13 +1,14 @@
 # Import libraries
 library(lavaan)
 library(tidyverse)
+library(arrow)
 
 #Some of the output can get long, so increasing the print output may be necessary
 options(max.print=99999)
 
-#If your data is a .csv, use this code below 
-#(replace "data.csv" with whatever your dataset is called)
-data<-read.csv("data.csv")
+# Set directory for output and load in dataset
+setwd("~/Desktop/Projects/Programming/PSYC499/scripts/r")
+data <- read_parquet("~/Desktop/Projects/Programming/PSYC499/data_clean/Dataset_clean_1.parquet")
 
 #The following lavaan code specifies the model
 #Replace the harm_block_ variables with the names of your timepoint variables
@@ -17,30 +18,29 @@ data<-read.csv("data.csv")
 # ~ is used to designate a path (i.e., regression) and ~~ is used to designate covariances
 
 sem_model<-'
-# This model the latent intercept for the slope (i.e., at initial timepoint). 
+# This models the latent intercept for the slope (i.e., at initial timepoint). 
 # Each gets multiplied by 1.
-intercept =~ 1*harm_block_1 + 1*harm_block_2 + 1*harm_block_3 + 1*harm_block_4 + 1*harm_block_5 + 1*harm_block_6 + 1*harm_block_7 + 1*harm_block_8 + 1*harm_block_9 + 1*harm_block_10 + 1*harm_block_11 + 1*harm_block_12 + 1*harm_block_13 + 1*harm_block_14 + 1*harm_block_15 + 1*harm_block_16
+intercept =~ 1+Y2017_QTR1+ 1*Y2017_QTR2 + 1*Y2017_QTR3 + 1*Y2017_QTR4 + 1*Y2018_QTR1 + 1*Y2018_QTR2 + 1*Y2018_QTR3 + 1*Y2018_QTR4 + 1*Y2019_QTR1 + 1*Y2019_QTR2 + 1*Y2019_QTR3 + 1*Y2019_QTR4 + 1*Y2020_QTR1 + 1*Y2020_QTR2 + 1*Y2020_QTR3 + 1*Y2020_QTR4
 
 # This models the latent slope. Note that each gets multipled by a coefficient 
 # that increases with each timepoint.
-slope =~ 0*harm_block_1+1*harm_block_2+2*harm_block_3+3*harm_block_4+4*harm_block_5+5*harm_block_6+6*harm_block_7+7*harm_block_8+8*harm_block_9+9*harm_block_10+10*harm_block_11+11*harm_block_12+12*harm_block_13+13*harm_block_14+14*harm_block_15+15*harm_block_16
+slope =~ 0*Y2017_QTR1+1*Y2017_QTR2+2*Y2017_QTR3+3*Y2017_QTR4+4*Y2018_QTR1+5*Y2018_QTR2+6*Y2018_QTR3+7*Y2018_QTR4+8*Y2019_QTR1+9*Y2019_QTR2+10*Y2019_QTR3+11*Y2019_QTR4+12*Y2020_QTR1+13*Y2020_QTR2+14*Y2020_QTR3+15*Y2020_QTR4
 
 # This will model the path from the vote to the intercept (replace vote with 
 # the variable name)
-intercept~vote
+intercept~Winning_Party
 
 # This will model the path from the vote to the slope
-slope~vote
+slope~Winning_Party
 
 # This will model the covariance between the latent intercept and the latent 
 # slope
 #This can tell you if counties that started higher in hate crimes also increased faster
 intercept ~~ slope
 '
-
 #If you don't know how to use R Markdown, you can use the function "sink" to create a text file of your output
 #This starts the sink
-sink(file = "model_output.txt")
+sink(file = "model_output1.txt")
 
 fit.growth_1<-growth(sem_model, data = data, se = "bootstrap", missing = "fiml", check.gradient = FALSE, control=list(iter.max=1000))
 summary(fit.growth_1,fit.measures = TRUE,standardized=T, ci=TRUE)
@@ -48,9 +48,3 @@ parameterEstimates(fit.growth_1)
 
 #This ends the sink
 sink(file = NULL)
-
-#Steps to consider for SEM
-#1: Does the model converge?
-#2: Does it show good fit?
-#The three most common fit indices are: RMSEA (> .08 is acceptable), TLI (>.80 is acceptable) and CFI (>.80 is acceptable)
-
